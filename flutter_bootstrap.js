dff386df0e359025981687a2248fd21c2392fc45
fandimetall <fandimetall@gmail.com>
2026-09-08 01:41:01 +0700

    deploy: update web build with auth session bridge

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3817459722"
+    serviceWorkerVersion: "1967918801"
   }
 });
