47ce613e9964f4509f93212c4e2381137ceb7141
fandimetall <fandimetall@gmail.com>
2026-09-07 18:36:58 +0700

    fix(auth): update web release

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "250247334"
+    serviceWorkerVersion: "1068838235"
   }
 });
