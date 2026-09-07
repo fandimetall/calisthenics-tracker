293f7ddaf5f1bf613e592f43aedb55ae1dc331b1
fandimetall <fandimetall@gmail.com>
2026-09-08 00:47:25 +0700

    deploy: update web build with tutorial video launchers

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1837107845"
+    serviceWorkerVersion: "3817459722"
   }
 });
