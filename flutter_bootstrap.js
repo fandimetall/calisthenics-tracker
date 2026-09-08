dfd5c5191cfa29b2c840bafae4b7974362a1fc4b
fandimetall <fandimetall@gmail.com>
2026-09-08 13:25:04 +0700

    deploy: update web build with back button fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "271419506"
+    serviceWorkerVersion: "2156809900"
   }
 });
